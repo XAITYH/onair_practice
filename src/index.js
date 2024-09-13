67ae4cd964c67c21d9e491c0d8b6e2869327d7d1
XAITYH <petrovskijpavel1@gmail.com>
2024-09-13 21:50:44 +0300

    Recommended Channels #1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const channel_cont = document.querySelectorAll('.channel_cont');
+const channel_conts = document.querySelectorAll(".channel_cont");
 
-for (i = 0; i < channel_cont.length; i++)
-channel_cont.innerHTML = `style:'background-image: url('../public/images/rec-channel-${i}.png')'`
+for (i = 0; i < channel_conts.length; i++)
+  channel_conts[i].style.backgroundImage = `url('../public/images/rec-channel-${i+1}.png')`;
