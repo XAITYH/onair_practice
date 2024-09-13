fd2ac1b8a4476313546f9f02a70d3e09c6e94be5
XAITYH <petrovskijpavel1@gmail.com>
2024-09-13 22:30:41 +0300

    hover effect rec-channel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const channel_conts = document.querySelectorAll(".channel_cont");
 
-for (i = 0; i < channel_conts.length; i++)
+for (i = 0; i < channel_conts.length; i++) {
+    if(i == 2)
+        continue
   channel_conts[i].style.backgroundImage = `url('../public/images/rec-channel-${i+1}.png')`;
+}
