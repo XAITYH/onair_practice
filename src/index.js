912d139d81253cadadc0ce32e6f2dc3a6c11d02d
XAITYH <petrovskijpavel1@gmail.com>
2024-09-17 21:45:55 +0300

    new-channels

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const channel_conts = document.querySelectorAll(".channel_cont");
-const rec_channels = document.querySelector(".rec_channels_cont")
+const new_channel_conts = document.querySelectorAll(".new-channel_cont");
 
 for (i = 0; i < channel_conts.length; i++) {
     if(i == 2)
         continue
   channel_conts[i].style.backgroundImage = `url('../public/images/rec-channel-${i+1}.png')`;
 }
+
+for (i = 0; i < new_channel_conts.length; i++)
+  new_channel_conts[i].style.backgroundImage = `url('../public/images/new-channel-${i+1}.png')`;
