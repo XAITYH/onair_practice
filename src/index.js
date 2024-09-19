4307965cffbeb25849feef2e47a9fd339ce5c79e
XAITYH <petrovskijpavel1@gmail.com>
2024-09-19 23:16:08 +0300

    new-movies containers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const channel_conts = document.querySelectorAll(".channel_cont");
 const new_channel_conts = document.querySelectorAll(".new-channel_cont");
 
-for (i = 0; i < channel_conts.length; i++) {
-    if(i == 2)
-        continue
-  channel_conts[i].style.backgroundImage = `url('../public/images/rec-channel-${i+1}.png')`;
+const new_movie_conts = document.querySelectorAll(".new-movie_cont");
+const popNumbers = [3, 1, 4, 5, null, 2, 6];
+
+for (let i = 0; i < channel_conts.length; i++) {
+  if (i == 2) continue;
+  channel_conts[i].style.backgroundImage = `url('../public/images/rec-channel-${
+    i + 1
+  }.png')`;
 }
 
-for (i = 0; i < new_channel_conts.length; i++)
-  new_channel_conts[i].style.backgroundImage = `url('../public/images/new-channel-${i+1}.png')`;
+for (let i = 0; i < new_channel_conts.length; i++)
+  new_channel_conts[
+    i
+  ].style.backgroundImage = `url('../public/images/new-channel-${i + 1}.png')`;
+
+for (let i = 0; i < new_movie_conts.length; i++) {
+  if(i == 4)
+    continue;
+
+  let pop_num = popNumbers[i];
+
+  new_movie_conts[i].style.backgroundImage = `url('../public/images/new-${
+    i + 1
+  }-pop-${pop_num}-movie.png')`;
+}
